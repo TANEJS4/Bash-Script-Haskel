1873876917073312106e51661329818d1ad03af0
Shivam Taneja <tanejs4@mills.cas.mcmaster.ca>
2018-02-24 20:23:41 -0500

    edited Readme file and copied Project file

---
+++ ProjectAnalyze.sh
@@ -0,0 +1,53 @@
+
+#!/bin/bash
+# part1
+function checkstatus()
+{
+git fetch origin
+echo $(git status)
+}
+
+a1=(git fetch origin)
+
+#part 2
+function redirectChanges()
+{
+git diff $a1 >> changes.log
+
+}
+#part 3
+function todo()
+{
+grep -r --exclude=todo.log "#TODO" $1 >> todo.log
+
+}
+#part 4
+function errors()
+{
+
+ghc -fno-code *.hs $1 &>> error.log
+}
+#part 5
+function feature()
+{
+while true
+do
+echo "Enter yes / no "
+read n
+if [ $n = "yes" ] ; then
+        git pull
+        git status
+        git diff HEAD
+        break
+elseif [ $n = "no" ]
+        break
+else
+    	echo "Please enter yes / no ONLY"
+        read phew
+fi
+done
+}
+
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# CS1XA3vgua
+# CS1XA3
+#TANEJS4
+
+reference : Chitwan Sharma and Kuber Khanna for 1st and 4th question
 
